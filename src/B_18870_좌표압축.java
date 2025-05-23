2e25cc1671e0ffdaddcf9c0f00f20555e7c89d7f
wlstj <22020306jinseoyeon@gmail.com>
2025-05-23 14:29:05 +0900

    v2: B_18870_좌표압축_map

--- src/B_18870_좌표압축.java
+++ src/B_18870_좌표압축.java
@@ -2,38 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class B_18870_좌표압축 {
-	static List<Integer> list;
-	static int[] numbers;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		list = new ArrayList<>();
 
 		int N = Integer.parseInt(br.readLine());
-		numbers = new int[N];
 
+		Set<Integer> set = new HashSet<>();
+		int[] numbers = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] tmp = new int[2000001];
 		for (int i = 0; i < N; i++) {
 			int num = Integer.parseInt(st.nextToken());
-			tmp[num + 1000000]++;
+			set.add(num);
 			numbers[i] = num;
 		}
 
-		for (int num : numbers) {
-			if (tmp[num + 1000000] > 1) {
-				tmp[num + 1000000]--;
-				continue;
-			}
-			list.add(num);
-		}
-
+		List<Integer> list = new ArrayList<>(set);
 		Collections.sort(list);
 
+		Map<Integer, Integer> map = new HashMap<>();
+
+		int idx = 0;
+		for (int num : list) {
+			map.put(num, idx);
+			idx++;
+		}
+		
 		StringBuilder sb = new StringBuilder();
 		for (int num : numbers) {
-			int idx = Collections.binarySearch(list, num);
-			sb.append(idx).append(" ");
+			int i = map.get(num);
+			sb.append(i).append(" ");
 		}
 		System.out.println(sb);
 	}
